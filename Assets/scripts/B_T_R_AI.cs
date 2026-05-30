35bd8f6e25155a5549c5650a1034e7ab15fa00ed
Telekrafter <artem.frolov2103@gmail.com>
2026-05-30 17:02:11 +0300

    //

--- Assets/scripts/B_T_R_AI.cs
+++ Assets/scripts/B_T_R_AI.cs
@@ -6,8 +6,8 @@ public class B_T_R_AI : MonoBehaviour
 {
     public Transform player_position;
     public float B_T_R_speed;
-    public float attack_range;
-    public float aggro_range;
+    public float attack_range = 3.5f;
+    public float aggro_range = 25f;
 
     private float last_attack_time;
     private bool is_attacking = false;
@@ -17,12 +17,31 @@ public class B_T_R_AI : MonoBehaviour
     private Animator animator;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        GameObject player = GameObject.FingGameObjectWithTag("Player");
+        player_position = player.transform;
     }
 
    
     void Update()
     {
+        float distance_to_player = Vector2.Distance(transform.position, player_position.position);
+        if (agro_range >= distance_to_player)
+        {
+            if (distance_to_player > attack_range)
+            { 
+            
+            
+            }
+            else
+            {
+                
+                
+                
+            }
+        }
         
+
+
     }
 }

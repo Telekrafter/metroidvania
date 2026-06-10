cf11579505f7c93d4bf2ae86741a0cd2eaf857ca
Telekrafter <artem.frolov2103@gmail.com>
2026-06-10 16:14:48 +0300

    .

--- Assets/scripts/B_T_R_AI.cs
+++ Assets/scripts/B_T_R_AI.cs
@@ -6,42 +6,71 @@ public class B_T_R_AI : MonoBehaviour
 {
     public Transform player_position;
     public float B_T_R_speed;
-    public float attack_range = 3.5f;
-    public float aggro_range = 25f;
+    public float attack_range;
+    public float aggro_range;
 
     private float last_attack_time;
     private bool is_attacking = false;
-    public float attack_cd;
+    public float knock_cd;
+    private bool is_aggr = false;
+    private bool is_dash;
+    private float dash_time;
 
     private Rigidbody2D rb;
     private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        GameObject player = GameObject.FingGameObjectWithTag("Player");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
         player_position = player.transform;
     }
 
    
     void Update()
     {
-        float distance_to_player = Vector2.Distance(transform.position, player_position.position);
-        if (agro_range >= distance_to_player)
+        if (is_dash == true)
         {
-            if (distance_to_player > attack_range)
-            { 
             
-            
-            }
-            else
-            {
-                
-                
-                
-            }
+
         }
+        float distance_to_player = Vector2.Distance(transform.position, player_position.position);
+        if (aggro_range >= distance_to_player)
+        {
+            is_aggr = true;
+        } 
+
+        if (last_attack_time + knock_cd <= Time.time)
+        {
+            if (attack_range >= distance_to_player)
+            {
+                attack();
+            }
+            else if (is_aggr == true)
+            {
+                move();
+            }
+
+
+        }
+        
         
 
 
     }
+    private void attack()
+    {
+        rb.velocity = Vector2.zero;
+        last_attack_time = Time.time;
+        Debug.Log("atck");
+    }
+    private void move()
+    {
+        is_dash = true;
+        Vector2 direction = (player_position.position - transform.position).normalized;
+        rb.velocity = direction * B_T_R_speed;
+        attack();
+
+
+    }
+   
 }

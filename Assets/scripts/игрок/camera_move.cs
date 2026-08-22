12b4ca4a91cd983ff7136935b8332c594690543d
Telekrafter <artem.frolov2103@gmail.com>
2026-08-22 16:26:24 +0300

    .

--- Assets/scripts/игрок/camera_move.cs
+++ Assets/scripts/игрок/camera_move.cs
@@ -6,11 +6,16 @@ public class camera_move : MonoBehaviour
 {
     public Transform player_pos;
     public Vector3 ofset;
+    public float dead_zone;
+    public float dead_zone_ofset;
 
     void LateUpdate()
     {
-        Vector3 pos = player_pos.position;
-        pos.z = -12f;
-        transform.position = Vector3.Lerp(transform.position, pos + ofset, Time.deltaTime * 2f);
+        Vector3 camera_pos = transform.position;
+        Vector3 pos = player_pos.position + ofset;
+        float diffY = pos.y - camera_pos.y - dead_zone_ofset;
+        //pos.x = pos.x - Mathf.Clamp(pos.x - camera_pos.x, - dead_zone.x, dead_zone.x);
+        pos.y = camera_pos.y + (diffY - Mathf.Clamp(diffY, -dead_zone, dead_zone));
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 5f);
     }
 }
